64840bb340ab7f00f29dd40401dd1306eb6e4da4
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-07-07 16:02:43 +0200

    Implement, document and test adjacency_matrix()

--- R/adjacency_matrix.R
+++ R/adjacency_matrix.R
@@ -4,37 +4,143 @@
 #' Converts an edges list to an adjacency matrix (also known as connectivity 
 #' matrix).
 #' 
-#' @param data a `data.frame` with the following three columns: `from` (edge), 
-#'   `to` (edge), and `weight` (value of the vertice).
+#' @param edges a `data.frame` with the following three columns: `from` (the 
+#'   first node of the edge), `to` (the second node of the edge), and `edge` 
+#'   (presence/absence of the edge).
 #' 
 #' @param lower a logical value. If `TRUE` (default), keep values in the lower 
-#'   triangle of the matrix. Otherwise their will be replaced by `NA`.
+#'   triangle of the matrix. Otherwise they will be replaced by `NA`.
 #' 
 #' @param upper a logical value. If `TRUE` (default), keep values in the upper 
-#'   triangle of the matrix. Otherwise their will be replaced by `NA`.
+#'   triangle of the matrix. Otherwise they will be replaced by `NA`.
 #' 
 #' @param diag a logical value. If `TRUE` (default), keep values in the 
-#'   diagonal of the matrix. Otherwise their will be replaced by `NA`.
+#'   diagonal of the matrix. Otherwise they will be replaced by `NA`.
+#'   
+#' @param na_to_zero a logical value. If `TRUE` (default), missing edges are 
+#'   coded as `0`. Otherwise they will be coded as `NA`.
 #'
-#' @return An integer vector of the same length as `x`.
+#' @return An adjacency matrix of dimensions `n x n`, where `n` is the number 
+#'   of nodes.
 #' 
 #' @export
 #' 
 #' @importFrom rlang .data
 #'
 #' @examples
-#' \dontrun{
-#' ## _Add an example_
-#' }
+#' # Import Adour sites ----
+#' path_to_file <- system.file("extdata", "adour_sites_coords.csv", 
+#'                             package = "bridge")
+#' adour_sites  <- read.csv(path_to_file)
+#' 
+#' # Find edges with 1 degree of neighborhood (undirected) ----
+#' adour_edges <- edges_list(adour_sites$"site")
+#' 
+#' # Get adjacency matrix ----
+#' adjacency_matrix(adour_edges)
+#' 
+#' # Get adjacency matrix ----
+#' adjacency_matrix(adour_edges, na_to_zero = FALSE)
+#' 
+#' # Find edges with 1 degree of neighborhood (directed) ----
+#' adour_edges <- edges_list(adour_sites$"site", directed = TRUE)
+#' 
+#' # Get adjacency matrix ----
+#' adjacency_matrix(adour_edges)
+#' 
+#' # Find edges with 1 degree of neighborhood (auto-links) ----
+#' adour_edges <- edges_list(adour_sites$"site", self = TRUE)
+#' 
+#' # Get adjacency matrix ----
+#' adjacency_matrix(adour_edges)
+#' 
+#' # Get adjacency matrix (remove upper triangle) ----
+#' adjacency_matrix(adour_edges, upper = FALSE)
+#' 
+#' # Get adjacency matrix (remove lower triangle) ----
+#' adjacency_matrix(adour_edges, lower = FALSE)
+#' 
+#' # Get adjacency matrix (remove diagonal) ----
+#' adjacency_matrix(adour_edges, diag = FALSE)
 
-adjacency_matrix <- function(data, lower = TRUE, upper = TRUE, diag = TRUE) {
+adjacency_matrix <- function(edges, lower = TRUE, upper = TRUE, diag = TRUE,
+                             na_to_zero = TRUE) {
   
-  colnames(data) <- c("from", "to", "weight")
-  mat <- tidyr::pivot_wider(data, names_from = .data$to, 
-                            values_from = .data$weight, values_fn = ~.x)
-  r_names <- mat[ , 1, drop = TRUE]
+  ## Check edges argument ----
+  
+  if (missing(edges)) {
+    stop("Argument 'edges' is required", call. = FALSE)
+  }
+  
+  if (!is.data.frame(edges)) {
+    stop("Argument 'edges' must be a data.frame", call. = FALSE)
+  }
+  
+  if (!("from" %in% colnames(edges))) {
+    stop("The column 'from' is absent from the edges data.frame", 
+         call. = FALSE)
+  }
+  
+  if (!("to" %in% colnames(edges))) {
+    stop("The column 'to' is absent from the edges data.frame", 
+         call. = FALSE)
+  }
+  
+  if (!("edge" %in% colnames(edges))) {
+    stop("The column 'edge' is absent from the edges data.frame", 
+         call. = FALSE)
+  }
+  
+  if (!("edge_id" %in% colnames(edges))) {
+    stop("The column 'edge_id' is absent from the edges data.frame", 
+         call. = FALSE)
+  }
+  
+  if (nrow(edges) == 0) {
+    stop("Argument 'edges' must have at least one row", call. = FALSE)
+  }
+  
+  
+  ## Complete missing edges ----
+  
+  nodes <- nodes_list(edges)[ , 1]
+  edges_all <- expand.grid(nodes, nodes, stringsAsFactors = FALSE)
+  colnames(edges_all) <- c("from", "to")
+  
+  edges_all$"key" <- paste0(edges_all$"from", edges_all$"to")
+  edges$"key"     <- paste0(edges$"from", edges$"to")
+  
+  edges <- merge(edges_all, edges[ , c("edge", "key")], by = "key", all = TRUE)
+  edges <- edges[ , c("from", "to", "edge")]
+  
+  
+  ## Replace missing edges ----
+  
+  if (na_to_zero) {
+    
+    edges$"edge" <- ifelse(is.na(edges$"edge"), 0, edges$"edge") 
+  
+  } else {
+    
+    edges$"edge" <- ifelse(is.na(edges$"edge") | edges$"edge" == 0, NA, 
+                           edges$"edge")
+  }
+  
+  
+  ## Pivot data frame ----
+  
+  mat <- tidyr::pivot_wider(edges, names_from = .data$to, 
+                            values_from = .data$edge, values_fn = ~.x)
+  
+  
+  ## Convert to matrix ----
+  
+  row_names <- mat[ , 1, drop = TRUE]
   mat <- data.matrix(mat[ , -1])
-  rownames(mat) <- r_names
+  rownames(mat) <- row_names
+  
+  
+  ## Apply filters ----
   
   if (!upper) mat[upper.tri(mat)] <- NA
   if (!lower) mat[lower.tri(mat)] <- NA
