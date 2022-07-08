33b6abaefda1aa947fb62d29b216bb16479260a4
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-07-08 08:31:46 +0200

    Do not export internal functions

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
-#' Convert POINTS to a LINESTRING
+#' Convert POINT to a LINESTRING
 #' 
 #' @description
 #' Converts spatial points of type `POINT` into a `LINESTRING`. This function
@@ -8,41 +8,31 @@
 #' @param points_sf an `sf` object of type `POINT`. Typically points sampled on
 #'   a LINESTRING as the output of [line_to_points()].
 #' 
-#' @param from an `integer` of length 1. The start of the segment (row number in 
-#'   `points_sf`).
+#' @param from an `integer` of length 1. The start of the segment (row number 
+#'   in `points_sf`).
 #'   
 #' @param to an `integer` of length 1. The end of the segment (row number in 
 #'   `points_sf`).
 #' 
-#' @export
+#' @noRd
 #' 
 #' @examples
 #' \dontrun{
-#' ## Import Adour river (LINESTRING) ----
-#' 
+#' # Import Adour river (LINESTRING) ----
 #' path_to_file <- system.file("extdata", "adour_lambert93.gpkg", 
 #'                             package = "bridge")
-#'                             
 #' adour_river <- sf::st_read(path_to_file)
 #' 
-#' 
-#' ## Sample points of the Adour river ----
-#' 
+#' # Sample points of the Adour river ----
 #' adour_river_pts <- line_to_points(adour_river)
 #' 
-#' 
-#' ## Convert back points to LINESTRING ----
-#' 
+#' # Convert back points to LINESTRING ----
 #' adour_river_all <- points_to_line(adour_river_pts, 1, nrow(adour_river_pts))
 #' 
-#' 
-#' ## Extract on segment on the river ----
-#' 
+#' # Extract on segment on the river ----
 #' adour_river_seg <- points_to_line(adour_river_pts, 100, 350)
 #' 
-#' 
-#' ## Map ----
-#' 
+#' # Map ----
 #' plot(sf::st_geometry(adour_river))
 #' plot(sf::st_geometry(adour_river_all), add = TRUE, col = "red")
 #' plot(sf::st_geometry(adour_river_seg), add = TRUE, col = "blue", lwd = 2)
@@ -122,4 +112,77 @@ points_to_line <- function(points_sf, from, to) {
     dplyr::summarise(do_union = FALSE) %>% 
     sf::st_cast("LINESTRING") %>% 
     dplyr::ungroup()
+}
+
+
+
+#' Convert a LINESTRING to POINT
+#' 
+#' @description
+#' Converts a spatial line of type `LINESTRING` into `POINT` by sampling points
+#' on a linear geometry using the function [sf::st_line_sample()]. This 
+#' function is used by the function [distance_along()].
+#' 
+#' @param x an `sf` object of type `LINESTRING`.
+#' 
+#' @inheritParams distance_along
+#' 
+#' @noRd
+#' 
+#' @examples
+#' \dontrun{
+#' # Import Adour river (LINESTRING) ----
+#' path_to_file <- system.file("extdata", "adour_lambert93.gpkg", 
+#'                             package = "bridge")
+#' adour_river <- sf::st_read(path_to_file)
+#' 
+#' # Sample points of the Adour river ----
+#' adour_river_pts <- line_to_points(adour_river)
+#' }
+
+line_to_points <- function(x, density = 0.01, type = "regular", ...) {
+  
+  ## Check 'x' argument ----
+  
+  if (missing(x)) {
+    stop("Argument 'x' (spatial coordinates of linear structure) is ", 
+         "required", call. = FALSE)
+  }
+  
+  if (!inherits(x, "sf")) {
+    stop("The object 'x' must be an 'sf' object", 
+         call. = FALSE)
+  }
+  
+  if (nrow(x) != 1) {
+    stop("Argument 'x' should have exactly one row", 
+         call. = FALSE)
+  }
+  
+  geom <- sf::st_geometry_type(x) %>% as.character() %>% unique()
+  
+  if (length(geom) > 1) {
+    stop("Argument 'x' (linear structure) cannot contain different ", 
+         "geometries", call. = FALSE)
+  }
+  
+  if (!("LINESTRING" %in% geom)) {
+    stop("Linear structure geometry must be of type LINESTRING", call. = FALSE)
+  }
+  
+  
+  ## Check 'type' argument ----
+  
+  if (!(type %in% c("regular", "random"))) {
+    stop("Argument 'type' must either 'regular' or 'random'")
+  }
+  
+  
+  ## Convert LINESTRING to POINT ----
+  
+  sf::st_line_sample(x, density = density, type = type, ...) %>% 
+    sf::st_sf() %>% 
+    sf::st_cast("POINT") %>% 
+    dplyr::mutate(id = 1:dplyr::n(), group = 1) %>%
+    dplyr::select(.data$id, .data$group)
 }
