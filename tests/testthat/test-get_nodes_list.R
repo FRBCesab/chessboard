1bf65b6cb7a7286b77905b790d0db867fcf37960
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-05-17 11:18:43 +0200

    tests: add get_nodes_list() tests

---
+++ tests/testthat/test-get_nodes_list.R
@@ -0,0 +1,27 @@
+# Data for tests ----
+
+sites <- expand.grid("transect" = 1:3, 
+                     "quadrat"  = 1:5)
+
+nodes <- create_nodes_labels(data     = sites, 
+                             transect = "transect", 
+                             quadrat  = "quadrat")
+
+
+# Tests for success ----
+
+test_that("get_nodes_list() - Tests for success", {
+  
+  expect_silent({ check <- get_nodes_list(nodes) })
+  
+  expect_equal(class(check), "character")
+  expect_equal(length(check), nrow(nodes))
+  
+  expect_equal(check[1], "1-1")
+  expect_equal(check[2], "1-2")
+  expect_equal(check[3], "1-3")
+  
+  expect_equal(check[13], "3-3")
+  expect_equal(check[14], "3-4")
+  expect_equal(check[15], "3-5")
+})
